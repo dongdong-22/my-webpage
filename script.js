ccc2244d4d5122974c91a56633e8e1e86b9cbbde
dongdong-22 <vor_zxcvbv72@naver.com>
2025-05-14 23:58:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function activateInput(box) {
-  const span = box.querySelector("span");
-  const input = box.querySelector("input");
+  const span = box.querySelector('span');
+  const input = box.querySelector('input');
   span.style.display = 'none';
   input.style.display = 'block';
   input.focus();
